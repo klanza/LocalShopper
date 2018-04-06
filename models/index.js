02b386b79b5ff0d502eb764264044283c3cb509a
klanza <klanza91@gmail.com>
2018-04-05 19:11:46 -0700

    Corrected NekoDB models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,10 @@
+const ko = require ('nekodb');
+
+ko.connect({
+  client: 'mongodb',
+  url: 'mongodb://localhost:27017/localshopper'
+})
+
 module.exports = {
     Product: require("./product"),
     User: require("./user")
