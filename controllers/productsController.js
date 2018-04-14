07d15b23b893d7e3819adc6257d48df8b53060ed
klanza <klanza91@gmail.com>
2018-04-14 10:15:09 -0700

    Added join to include User function

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -5,7 +5,7 @@ const db = require("../models");
 module.exports = {
   findAll: function(req, res) {
     db.Product
-      .find(req.query)
+      .find(req.query).join()
       // .sort({ _id: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
